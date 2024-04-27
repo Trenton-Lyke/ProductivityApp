f3101921cf34efc85a71eaf9b7e216ecb546ce0d
Trenton Lyke <tjl223@cornell.edu>
2024-04-27 10:54:20 -0400

    Initial Commit

---
+++ Productivity/ProductivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityApp.swift
+//  Productivity
+//
+//  Created by Trenton Lyke on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
