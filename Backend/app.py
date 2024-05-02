046f9b158031a26335e58a53ccf324284669d1fb
Trenton Lyke <tjl223@cornell.edu>
2024-05-01 23:13:26 -0400

    implemented timer and leader board endpoints and changed backend to match more with frontend

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,9 @@
 import json
+import datetime
 
-from db import Assignment, User, Course, db
+from db import Assignment, User, Course, Timer, db
+from sqlalchemy import func
+from sqlalchemy.sql import text
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -45,23 +48,23 @@ def create_course():
     Endpoint for creating a new course
     """
     body = json.loads(request.data)
-    code = body.get("code", None)
     name = body.get("name", None)
+    description = body.get("description", None)
 
     error = ""
 
-    if code is None:
-        error += "Missing code in request body. "
-
     if name is None:
         error += "Missing name in request body. "
+
+    if description is None:
+        error += "Missing description in request body. "
 
     error = error.strip()
 
     if error != "":
         return failure_response(error, 400)
 
-    new_course = Course(code=code, name=name)
+    new_course = Course(name=name, description=description)
     db.session.add(new_course)
     db.session.commit()
     return success_response(new_course.serialize(), 201)
@@ -98,22 +101,18 @@ def create_user():
     """
     body = json.loads(request.data)
     name = body.get("name", None)
-    netid = body.get("netid", None)
 
     error = ""
 
     if name is None:
         error += "Missing name in request body. "
 
-    if netid is None:
-        error += "Missing netid in request body. "
-
     error = error.strip()
 
     if error != "":
         return failure_response(error, 400)
 
-    new_user = User(name=name, netid=netid)
+    new_user = User(name=name)
     db.session.add(new_user)
     db.session.commit()
     return success_response(new_user.serialize(), 201)
@@ -134,17 +133,11 @@ def get_user(user_id):
 def add_user_to_course(course_id):
     body = json.loads(request.data)
     user_id = body.get("user_id", None)
-    type = body.get("type", None)
 
     error = ""
 
     if user_id is None:
         error += "Missing user_id in request body. "
-
-    if type is None:
-        error += "Missing type in request body."
-    elif type != "student" and type != "instructor":
-        error += "type must be student or instructor"
 
     error = error.strip()
 
@@ -159,10 +152,7 @@ def add_user_to_course(course_id):
     if course is None:
         return failure_response("Course not found")
 
-    if type == "instructor":
-        course.instructors.append(user)
-    else:
-        course.students.append(user)
+    course.users.append(user)
 
     db.session.commit()
     return success_response(course.serialize())
@@ -171,16 +161,24 @@ def add_user_to_course(course_id):
 @app.route("/api/courses/<int:course_id>/assignment/", methods=["POST"])
 def add_assignment_to_course(course_id):
     body = json.loads(request.data)
-    title = body.get("title", None)
+    name = body.get("name", None)
+    description = body.get("description", None)
     due_date = body.get("due_date", None)
+    done = body.get("done", None)
 
     error = ""
 
-    if title is None:
-        error += "Missing title in request body. "
+    if name is None:
+        error += "Missing name in request body. "
+
+    if description is None:
+        error += "Missing description in request body. "
 
     if due_date is None:
         error += "Missing due_date in request body. "
+
+    if done is None:
+        error += "Missing done in request body. "
 
     error = error.strip()
 
@@ -191,11 +189,164 @@ def add_assignment_to_course(course_id):
     if course is None:
         return failure_response("Course not found")
 
-    assignment = Assignment(title=title, due_date=due_date, course_id=course_id)
+    assignment = Assignment(
+        name=name,
+        description=description,
+        due_date=due_date,
+        done=done,
+        course_id=course_id,
+    )
     db.session.add(assignment)
     db.session.commit()
 
     return success_response(assignment.serialize(), 201)
+
+
+@app.route("/api/timer/", methods=["POST"])
+def add_timer_to_user():
+    body = json.loads(request.data)
+    elapsed_time = body.get("elapsed_time", None)
+    hours = body.get("hours", None)
+    minutes = body.get("minutes", None)
+    seconds = body.get("seconds", None)
+    date = body.get("date", None)
+    user_id = body.get("user_id", None)
+
+    error = ""
+
+    if elapsed_time is None:
+        error += "Missing elapsed_time in request body. "
+    if hours is None:
+        error += "Missing hours in request body. "
+    if minutes is None:
+        error += "Missing minutes in request body. "
+    if seconds is None:
+        error += "Missing seconds in request body. "
+    if date is None:
+        error += "Missing date in request body. "
+    if user_id is None:
+        error += "Missing user_id in request body. "
+
+    error = error.strip()
+
+    if error != "":
+        return failure_response(error, 400)
+
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found")
+
+    timer = Timer(
+        elapsed_time=elapsed_time,
+        hours=hours,
+        minutes=minutes,
+        seconds=seconds,
+        date=date,
+        user_id=user_id,
+    )
+    db.session.add(timer)
+    db.session.commit()
+
+    return success_response(timer.serialize(), 201)
+
+
+@app.route("/api/timer/<int:user_id>/")
+def get_timers(user_id):
+    """
+    Endpoint for getting timers from specific user
+    """
+    timers = Timer.query.filter_by(user_id=user_id).all()
+    print(timers)
+    if timers is None:
+        return failure_response("User not found")
+    return success_response([t.serialize() for t in timers])
+
+
+@app.route("/api/timer/leader_board/<time_span>/")
+def get_leader_board(time_span):
+    """
+    Endpoint for work time leader board
+    """
+    if (
+        time_span != "all"
+        and time_span != "day"
+        and time_span != "week"
+        and time_span != "month"
+        and time_span != "year"
+    ):
+        return failure_response(
+            f"The time_span must be all, day, week, month, or year not {time_span}",
+            code=400,
+        )
+
+    leader_board = get_leader_board(time_span)
+
+    if leader_board is None:
+        return failure_response("No timers found")
+    return success_response(
+        [{"user_id": l.user_id, "total": l.total} for l in leader_board]
+    )
+
+
+@app.route("/api/timer/leader_board/<time_span>/<int:user_id>/")
+def get_leader_board_position(time_span, user_id):
+    """
+    Endpoint for user's position in work time leader board
+    """
+    if (
+        time_span != "all"
+        and time_span != "day"
+        and time_span != "week"
+        and time_span != "month"
+        and time_span != "year"
+    ):
+        return failure_response(
+            f"The time_span must be all, day, week, month, or year not {time_span}",
+            code=400,
+        )
+
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found")
+
+    leader_board = [l.user_id for l in get_leader_board(time_span)]
+
+    if leader_board is None:
+        return failure_response("No timers found")
+
+    if user_id in leader_board:
+        return success_response({"position": leader_board.index(user_id) + 1})
+    else:
+        return success_response({"position": len(leader_board) + 1})
+
+
+def get_leader_board(time_span):
+    """
+    Helper function for getting work time leader board
+    """
+    query = db.session.query(Timer.user_id, func.sum(Timer.elapsed_time).label("total"))
+
+    leader_board = []
+    if time_span == "all":
+        leader_board = query.group_by(Timer.user_id).order_by(text("total DESC")).all()
+    else:
+        min_date = datetime.datetime.now().timestamp()
+        if time_span == "day":
+            min_date -= 60 * 60 * 24
+        elif time_span == "week":
+            min_date -= 60 * 60 * 24 * 7
+        elif time_span == "month":
+            min_date -= 60 * 60 * 24 * 31
+        elif time_span == "year":
+            min_date -= 60 * 60 * 24 * 365
+        leader_board = (
+            query.filter(Timer.date >= min_date)
+            .group_by(Timer.user_id)
+            .order_by(text("total DESC"))
+            .all()
+        )
+
+    return leader_board
 
 
 if __name__ == "__main__":
--- Backend/db.py
+++ Backend/db.py
@@ -2,15 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-student_association_table = db.Table(
-    "student_association",
-    db.Model.metadata,
-    db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
-    db.Column("course_id", db.Integer, db.ForeignKey("course.id")),
-)
-
-instructor_association_table = db.Table(
-    "instructor_association",
+association_table = db.Table(
+    "association",
     db.Model.metadata,
     db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
     db.Column("course_id", db.Integer, db.ForeignKey("course.id")),
@@ -26,12 +19,9 @@ class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
-    netid = db.Column(db.String, nullable=False)
-    student_courses = db.relationship(
-        "Course", secondary=student_association_table, back_populates="students"
-    )
-    instructor_courses = db.relationship(
-        "Course", secondary=instructor_association_table, back_populates="instructors"
+    timers = db.relationship("Timer", cascade="delete")
+    courses = db.relationship(
+        "Course", secondary=association_table, back_populates="users"
     )
 
     def __init__(self, **kwargs):
@@ -39,13 +29,12 @@ class User(db.Model):
         Initialize User Entry
         """
         self.name = kwargs.get("name")
-        self.netid = kwargs.get("netid")
 
     def simple_serialize(self):
         """
         Serialize User object without courses
         """
-        return {"id": self.id, "name": self.name, "netid": self.netid}
+        return {"id": self.id, "name": self.name}
 
     def serialize(self):
         """
@@ -53,8 +42,8 @@ class User(db.Model):
         """
         return {
             **self.simple_serialize(),
-            "courses": [c.simple_serialize() for c in self.student_courses]
-            + [c.simple_serialize() for c in self.instructor_courses],
+            "courses": [c.simple_serialize() for c in self.courses],
+            "timers": [t.simple_serialize() for t in self.timers],
         }
 
 
@@ -65,30 +54,25 @@ class Course(db.Model):
 
     __tablename__ = "course"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    code = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
     assignments = db.relationship("Assignment", cascade="delete")
-    instructors = db.relationship(
-        "User",
-        secondary=instructor_association_table,
-        back_populates="instructor_courses",
-    )
-    students = db.relationship(
-        "User", secondary=student_association_table, back_populates="student_courses"
+    users = db.relationship(
+        "User", secondary=association_table, back_populates="courses"
     )
 
     def __init__(self, **kwargs):
         """
         Initialize Course Object
         """
-        self.code = kwargs.get("code")
         self.name = kwargs.get("name")
+        self.description = kwargs.get("description")
 
     def simple_serialize(self):
         """
-        Serialize Course without assignments, instructors, or students
+        Serialize Course without assignments or users
         """
-        return {"id": self.id, "code": self.code, "name": self.name}
+        return {"id": self.id, "name": self.name, "description": self.description}
 
     def serialize(self):
         """
@@ -97,8 +81,7 @@ class Course(db.Model):
         return {
             **self.simple_serialize(),
             "assignments": [a.simple_serialize() for a in self.assignments],
-            "instructors": [i.simple_serialize() for i in self.instructors],
-            "students": [s.simple_serialize() for s in self.students],
+            "users": [u.simple_serialize() for u in self.users],
         }
 
 
@@ -109,8 +92,10 @@ class Assignment(db.Model):
 
     __tablename__ = "assignment"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
     due_date = db.Column(db.Integer, nullable=False)
+    done = db.Column(db.Boolean, nullable=False)
     course_id = db.Column(db.Integer, db.ForeignKey("course.id"), nullable=False)
     course = db.relationship("Course", foreign_keys="Assignment.course_id")
 
@@ -118,18 +103,72 @@ class Assignment(db.Model):
         """
         Initialize Assignment Object
         """
-        self.title = kwargs.get("title")
+        self.name = kwargs.get("name")
+        self.description = kwargs.get("description")
         self.due_date = kwargs.get("due_date")
+        self.done = kwargs.get("done")
         self.course_id = kwargs.get("course_id")
 
     def simple_serialize(self):
         """
         Serialize Assignment without course
         """
-        return {"id": self.id, "title": self.title, "due_date": self.due_date}
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "due_date": self.due_date,
+            "done": self.done,
+        }
 
     def serialize(self):
         """
         Serialize Assignment
         """
         return {**self.simple_serialize(), "course": self.course.simple_serialize()}
+
+
+class Timer(db.Model):
+    """
+    Timer Model
+    """
+
+    __tablename__ = "timer"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    elapsed_time = db.Column(db.Integer, nullable=False)
+    hours = db.Column(db.Integer, nullable=False)
+    minutes = db.Column(db.Integer, nullable=False)
+    seconds = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user = db.relationship("User", foreign_keys="Timer.user_id")
+
+    def __init__(self, **kwargs):
+        """
+        Initialize Assignment Object
+        """
+        self.elapsed_time = kwargs.get("elapsed_time")
+        self.hours = kwargs.get("hours")
+        self.minutes = kwargs.get("minutes")
+        self.seconds = kwargs.get("seconds")
+        self.date = kwargs.get("date")
+        self.user_id = kwargs.get("user_id")
+
+    def simple_serialize(self):
+        """
+        Serialize Assignment without course
+        """
+        return {
+            "id": self.id,
+            "elapsed_time": self.elapsed_time,
+            "hours": self.hours,
+            "minutes": self.minutes,
+            "seconds": self.seconds,
+            "date": self.date,
+        }
+
+    def serialize(self):
+        """
+        Serialize Assignment
+        """
+        return {**self.simple_serialize(), "user": self.user.simple_serialize()}
